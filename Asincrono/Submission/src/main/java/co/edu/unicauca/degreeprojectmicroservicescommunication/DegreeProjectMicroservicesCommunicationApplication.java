94dcd47f05389ecccec03531cc28706c1c71ae70
Juan Ortega <jmo.narvaez@gmail.com>
2025-10-11 11:44:43 -0500

    ✨ init: proyecto base Spring Boot para comunicación síncrona y asíncrona

---
+++ Asincrono/Submission/src/main/java/co/edu/unicauca/degreeprojectmicroservicescommunication/DegreeProjectMicroservicesCommunicationApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.degreeprojectmicroservicescommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreeProjectMicroservicesCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DegreeProjectMicroservicesCommunicationApplication.class, args);
+    }
+
+}
---
+++ Sincrono/Notification/src/test/java/co/edu/unicauca/degreeprojectmicroservicescommunication/DegreeProjectMicroservicesCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.degreeprojectmicroservicescommunication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreeProjectMicroservicesCommunicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
