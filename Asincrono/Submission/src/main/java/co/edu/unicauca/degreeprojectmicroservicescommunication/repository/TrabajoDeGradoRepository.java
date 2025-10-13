4ee6047b931996218ca9300636e62e41735763f4
LauraMolano <lauraisamolano@unicauca.edu.co>
2025-10-12 21:46:37 -0500

    🔥 Se removió interfaz no usada

--- Asincrono/Submission/src/main/java/co/edu/unicauca/degreeprojectmicroservicescommunication/repository/TrabajoDeGradoRepository.java
+++
@@ -1,7 +0,0 @@
-package co.edu.unicauca.degreeprojectmicroservicescommunication.repository;
-
-import co.edu.unicauca.degreeprojectmicroservicescommunication.entity.TrabajoDeGrado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrabajoDeGradoRepository extends JpaRepository<TrabajoDeGrado, Long> {
-}
